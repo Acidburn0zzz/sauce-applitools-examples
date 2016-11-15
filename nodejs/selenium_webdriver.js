41d9ab9e28cdac561c530e2e842de875a639a5a9
ndmanvar <ndmanvar@yahoo.com>
2016-11-14 22:27:44 -0600

    add basic js example

---
+++ nodejs/selenium_webdriver.js
@@ -0,0 +1,33 @@
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder()
+    .withCapabilities({
+        'browserName': 'firefox'
+    })
+    .usingServer("https://YOUR_SAUCE_USERNAME:YOUR_SAUCE_ACCESS_KEY@ondemand.saucelabs.com:443/wd/hub")
+    .build();
+
+var Eyes = require('eyes.selenium').Eyes;
+var eyes = new Eyes();
+// This is your api key, make sure you use it in all your tests.
+eyes.setApiKey('YOUR_API_KEY');
+
+// Start visual testing with browser viewport set to 1024x768.
+// Make sure to use the driver returned through 'then' from this point on.
+eyes.open(driver, 'Applitools', 'Test Web Page', {width: 1024, height: 768})
+    .then(function(driver) {
+        driver.get('http://applitools.com');
+
+        // Visual validation point #1
+        eyes.checkWindow('Main Page');
+
+        driver.findElement(webdriver.By.css('.features>a')).click();
+
+        // Visual validation point #2
+        eyes.checkWindow('Features page');
+
+        driver.quit();
+
+        // End visual testing. Validate visual correctness.
+        eyes.close();
+    });
